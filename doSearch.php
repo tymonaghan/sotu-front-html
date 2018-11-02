fcef607fedea858f1074e75d2064d6644c031dc7
Tyler Monaghan <ty.monaghan@gmail.com>
2018-11-02 00:17:44 -0500

    failure is just another step to success

--- doSearch.php
+++ doSearch.php
@@ -1,21 +1,5 @@
 <html>
 <body>
-<?php
-$term = null;
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-  $term = clean_input($_GET["searchTerm"]);
-
-}
-
-function clean_input($data) {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-return $data;
-}
-echo <p>You searched for . $term .</p>;
-# Your email address is: <?php echo $_GET["email"];
-
-?>
+You searched for <?php echo $_GET["searchTerm"]; ?>
 </body>
 </html>
