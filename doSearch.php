3421da5ddf0b7860aff07ea2577f405a09783609
Tyler Monaghan <ty.monaghan@gmail.com>
2018-11-01 23:28:40 -0500

    try again

--- doSearch.php
+++ doSearch.php
@@ -4,5 +4,16 @@
 You searched for <?php echo $_GET["searchTerm"]; ?><br>
 # Your email address is: <?php echo $_GET["email"]; ?>
 
+php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  $term = clean_input($_GET["searchTerm"]);
+}
+function clean_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
+}
+?>
 </body>
 </html>
