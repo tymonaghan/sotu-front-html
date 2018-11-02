58e48ca9cb7244898692e30fdc04717c024c6772
Tyler Monaghan <ty.monaghan@gmail.com>
2018-11-02 16:58:43 -0500

    now echoes back contents of file being created

--- php/text-to-file.php
+++ php/text-to-file.php
@@ -1,6 +1,10 @@
 <?php
-$output = "testTEXT"; # store the string "testTEXT" as $output
 file_put_contents("../sentiments.md","Text is getting appended then a line break \n", FILE_APPEND);
+#non-append version (overwrite) on below line, uncomment to use
+#file_put_contents("../sentiments.md","Text is getting appended then a line break \n"/*, FILE_APPEND*/);
+
 #replace fopen, fwrite, and fclose with file_put_contents
 # the parameters are (file to open/write, data to write, flags [FILE_APPEND] writes in append instead of overwrite mode
+$output = fopen("../sentiments.md","r");
+echo fread($output,filesize("../sentiments.md"));
 ?>
