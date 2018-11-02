6dbb0ddfef19550c8823c37d3fe88bbf7d4ffaa1
Tyler Monaghan <ty.monaghan@gmail.com>
2018-11-02 02:03:20 -0500

    read comments for php functionality notes

--- rscript.php
+++ rscript.php
@@ -1,8 +1,10 @@
 <?php
-$myFile = fopen("sentiments.txt", "w");
-$myScript = fopen("myscript.R", "r");
-fread($myScript,filesize("myscript.R"));
-fclose($myScript);
-fclose($myFile)
+$myFile = fopen("sentiments.txt", "w"); #opens (and creates if necessary) sentiments.txt as $myFile
+$myScript = fopen("myscript.R", "r"); # opens myscript.R as $myScript
+fwrite($myFile, fread($myScript,filesize("myscript.R"))); #writes ($to myFile, readscontent of $myScript for as many bytes as myscript.R is long)
+fclose($myScript); #close $myScript
+fclose($myFile); #close $myFile
 # Rscript myscript.R > sentiments.txt
+
+#ok well this version creates sentiments.txt and writes the myscript.R into it. just need to get it to write the output, not the script itself, and we're good to go
 ?>
