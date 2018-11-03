ee9eb5673df8ba302241d58cc6bd6687caa054f9
Tyler Monaghan <ty.monaghan@gmail.com>
2018-11-02 23:03:53 -0500

    change \n to <br> to render correctly in browser

--- php/text-to-file.php
+++ php/text-to-file.php
@@ -1,7 +1,7 @@
 <?php
-file_put_contents("../sentiments.md","Text is getting appended then a line break \n", FILE_APPEND);
+file_put_contents("../sentiments.md","Text is getting appended then a line break <br><br>", FILE_APPEND);
 #non-append version (overwrite) on below line, uncomment to use
-#file_put_contents("../sentiments.md","Text is getting appended then a line break \n"/*, FILE_APPEND*/);
+#file_put_contents("../sentiments.md","Text is getting appended then a line break <br>"/*, FILE_APPEND*/);
 
 #replace fopen, fwrite, and fclose with file_put_contents
 # the parameters are (file to open/write, data to write, flags [FILE_APPEND] writes in append instead of overwrite mode
